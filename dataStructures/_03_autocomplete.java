802fe1b11a0cba5f8f4cdf104a63dbcfaf0bc8ea
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-03-10 23:01:02 +0530

    auto complete done

--- dataStructures/_03_autocomplete.java
+++ dataStructures/_03_autocomplete.java
@@ -8,18 +8,30 @@ import java.util.List;
 public class _03_autocomplete {
 
     public static void main(String[] args) {
-        Trie trie=new Trie();
+
 
         List<String>data=List.of("app","apple","application","apply");
 
-        for(String str:data) trie.insert(str);
+        AutoCommpleteService service=new AutoCommpleteService(new FrequencyRecommendationStrategy());
+
+        for(String str:data) service.insert(str);
 
 
-        System.out.println(trie.getRecommendations("appl"));
+        System.out.println(service.getRecommendations("app",2));
 
-        trie.select("apply");
+        System.out.println(service.getRecommendations("apple",2));
 
-        System.out.println(trie.getRecommendations("app"));
+        service.insert("appa");
+
+        System.out.println(service.getRecommendations("app", 10));
+
+
+
+
+
+       /*[[app, searches=1], [apple, searches=1]]
+        [[app, searches=2], [apple, searches=2]]
+        [[app, searches=3], [apple, searches=3], [application, searches=3], [apply, searches=3], [appa, searches=1]]*/
 
 
 
@@ -27,7 +39,7 @@ public class _03_autocomplete {
     }
 }
 
-class Recommendation implements Comparable<Recommendation>{
+class Recommendation {
     private final String s;
     private final int count;
     public int getCount() {
@@ -41,10 +53,7 @@ class Recommendation implements Comparable<Recommendation>{
     public String toString() {
         return "["+s+", searches="+count+"]";
     }
-    @Override
-    public int compareTo(Recommendation o) {
-        return Integer.compare(o.getCount(),this.count);
-    }
+
     
 }
 
@@ -61,15 +70,15 @@ class Node{
         return val;
     }
     private Node[]ptrs;
-    private int count;
+    private int frequency;
 
-    public int getCount() {
-        return count;
+    public int getFrequency() {
+        return frequency;
     }
     public Node(char val) {
         this.val=val;
         this.ptrs=new Node[26];
-        this.count=0;
+        this.frequency=0;
         this.isEnd=false;
     }
     void setNodeFor(char c){
@@ -82,16 +91,21 @@ class Node{
         return ptrs[c-'a'];
     }
     void incr(){
-        count++;
+        frequency++;
     }
     @Override
     public String toString() {
-        return "{"+val+"=>"+count+"}";
+        return "{"+val+"=>"+frequency+"}";
     }
 }
 
 class Trie{
     private final Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
 
     public Trie() {
         root=new Node('#');
@@ -108,42 +122,59 @@ class Trie{
             ptr=next;
         }
         ptr.setIsEnd();
+    }    
+}   
+
+
+class AutoCommpleteService{
+    private final Trie trie;
+    private final RecommendationStrategy strategy;
+    public AutoCommpleteService(RecommendationStrategy strategy) {
+        trie=new Trie();
+        this.strategy=strategy;
+    }
+    public void insert(String s){
+        trie.insert(s);
+    }
+    public List<Recommendation> getRecommendations(String s,int limit){
+        return strategy.getRecommendation(s, trie, limit);
     }
 
-    public Node getEnd(String s){
-        char arr[]=s.toCharArray();
-        int i=0;
-        Node ptr=root;    
-        while(ptr!=null && i<arr.length){
-            Node next=ptr.getNodeFor(arr[i++]);
-            ptr=next;
-        }
-        return ptr;
-    }
+}
 
-    public List<Recommendation>getRecommendations(String s){
+
+interface RecommendationStrategy{
+    List<Recommendation>getRecommendation(String word,Trie trie,int limit);//can use PQ for better optimizations too
+}
+
+class FrequencyRecommendationStrategy implements RecommendationStrategy{//infuture can extend to MLStrategy,UserStrategy etc
+
+    @Override
+    public List<Recommendation> getRecommendation(String word, Trie trie,int limit) {
         List<Recommendation>ans=new ArrayList<>();
-        build(ans, "", getEnd(s));
-        Collections.sort(ans);
-        return ans;
+        collectRecommendations(ans, "", trie.getRoot());
+        Collections.sort(ans,(o1, o2) ->   o2.getCount()-o1.getCount());
+        return ans.subList(0, Math.min(ans.size(), limit));
     }
 
-    public void select(String s){
-        getEnd(s).incr();
-    }
+     
 
-    public void build(List<Recommendation>ans,String s,Node root){
+    
+    private void collectRecommendations(List<Recommendation>ans,String s,Node root){
         if(root==null){
             return;
         }
         if(root.isEnd()){
-            ans.add(new Recommendation(s, root.getCount()));
+            root.incr();//increment frequency of search of this word
+            ans.add(new Recommendation(s, root.getFrequency()));
+            //MISTAKE: added `return` here
         }
         for(char ch='a';ch<='z';ch++) {
             if(root.hasNodeFor(ch)){
-                build(ans, s+ch, root.getNodeFor(ch));
+                collectRecommendations(ans, s+ch, root.getNodeFor(ch));
             }
         }
     }
 
-}   
+    
+}
