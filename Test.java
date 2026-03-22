1ad4d394f580a0b6d0e3afe2035256c448f2a05c
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-03-22 23:41:39 +0530

    test thenApply()

--- Test.java
+++ Test.java
@@ -1,25 +1,32 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class Test {
 
-    public static void main(String[] args) {
-        List<Integer>li=new Random().ints(10000000l).boxed().collect(Collectors.toList());
-        long st=System.nanoTime();
-        double pSum=li.parallelStream().map(e->Math.sqrt(e)).reduce(0d, (x,y)->x+y);
-        long en=System.nanoTime();
-        System.out.println((en-st)/(double)1e6);//takes 122.39 ms
 
-        st=System.nanoTime();
-        double seqSum=li.stream().map(e->Math.sqrt(e)).reduce(0d, (x,y)->x+y);
-        en=System.nanoTime();
-        System.out.println((en-st)/(double)1e6);//takes 238.31 ms
 
-        //see parrallel ops are faster
 
+    static CompletableFuture<Integer> create(int x){
+        return CompletableFuture.supplyAsync(()->{
+            sleep(2333);
+            return x;
+        });
+    }
+
+    static void sleep(int dur){
+        try{System.out.println("sleep: "+Thread.currentThread().getName());Thread.sleep(dur);}catch(Exception e){}
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        CompletableFuture<Integer>cf=create(34).    
+        thenApply((x)->{return x*2;});
+
+
+        cf.thenAccept((x)->{sleep(4333);System.out.println(x);});
+
+        System.out.println("hi");
+
+
+        cf.join();
     }
 }
