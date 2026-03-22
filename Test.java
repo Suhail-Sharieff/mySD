8d8bbdba0118cc122c18adc32a61ade7f6f66cca
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-03-22 17:31:58 +0530

    test deopist withdrw

--- Test.java
+++ Test.java
@@ -1,49 +1,49 @@
 public class Test {
 
+    static int balance = 500;
 
-    static final Object obj=new Object();
     public static void main(String[] args) throws InterruptedException {
-        
-        Thread holder=new Thread(()->{
-            synchronized(obj){
-                System.out.println("holder holding resource");
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Holder releasing lock");
+
+        Thread t1 = new Thread(() -> {
+            try {
+                Thread.sleep(4000);
+                withDraw();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-        });
-        Thread capturer=new Thread(()->{
-             synchronized(obj){
-                System.out.println("capturer holding resource");
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("capturer releasing resource");
+        }, "withdrawer");
+        Thread t2 = new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+                deposit();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-        });
+        }, "depositor");
 
+        t1.start();
+        t2.start();
 
-        holder.start();
-        Thread.sleep(300);
-        System.out.println(holder.getState());//timed state 
-        capturer.start();
-        Thread.sleep(300);//blocked 
+        t1.join();
+        t2.join();
+        ;
 
-
-        System.out.println(capturer.getState());
-
-        holder.join();
-        capturer.join();;
-
-
-        //story: holder statrs, locks obj for 2s, simultaneously after 0.3s capturer tries to acquire it, but it will be blocked, acquires the same obj when captureer releases it
+        System.out.println(balance);
 
     }
 
+    static void withDraw() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+        Thread.sleep(300);
+        balance -= 100;
+    }
+
+    static void deposit() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+        Thread.sleep(200);
+        balance += 200;
+    }
 
 }
