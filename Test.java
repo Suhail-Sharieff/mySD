1f48e8f7f617d5322037c0d222b1c41d51ed8535
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-03-23 23:47:30 +0530

    learnt abt java.concurrent package

--- Test.java
+++ Test.java
@@ -1,32 +1,50 @@
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
 public class Test {
 
+     public static void main(String[] args) {
+        int nums[]={1,2,3,4,5,6,7,8,9,10};
+        Summer summer=new Summer(nums, 0, nums.length-1);
+        System.out.println(ForkJoinPool.commonPool().invoke(summer));
+     }
+
+}
 
 
+class Summer extends RecursiveTask<Integer>{
 
-    static CompletableFuture<Integer> create(int x){
-        return CompletableFuture.supplyAsync(()->{
-            sleep(2333);
-            return x;
-        });
+    private final int nums[];
+    private final int from;
+    private final int to;
+    private final int thrshHold=3;
+    
+
+    public Summer(int[] nums, int from, int to) {
+        this.nums = nums;
+        this.from = from;
+        this.to = to;
     }
 
-    static void sleep(int dur){
-        try{System.out.println("sleep: "+Thread.currentThread().getName());Thread.sleep(dur);}catch(Exception e){}
+
+
+    @Override
+    protected Integer compute() {
+        int len=to-from;
+        if(len<=thrshHold){
+            int sum=0;
+            for(int i=from;i<=to;i++) sum+=nums[i];
+            return sum;
+        }
+        int mid=(from+to)>>1;
+        Summer left=new Summer(nums, from, mid);
+        Summer right=new Summer(nums, mid+1, to);
+        left.fork();
+        right.fork();
+        int leftResult=left.join();System.out.println("["+from+","+mid+" via "+Thread.currentThread()+"]");
+        int rightResult=right.join();System.out.println("["+mid+","+to+" via "+Thread.currentThread()+"]");
+        return leftResult+rightResult;
     }
-
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
-        CompletableFuture<Integer>cf=create(34).    
-        thenApply((x)->{return x*2;});
-
-
-        cf.thenAccept((x)->{sleep(4333);System.out.println(x);});
-
-        System.out.println("hi");
-
-
-        cf.join();
-    }
+    
 }
