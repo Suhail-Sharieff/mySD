73acff7c03dcdcbb5867a5c6b0bec247b6b7155b
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-03-24 19:23:56 +0530

    learnt abt virtual threads

--- Test.java
+++ Test.java
@@ -1,50 +1,27 @@
-import java.util.Arrays;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 public class Test {
 
-     public static void main(String[] args) {
-        int nums[]={1,2,3,4,5,6,7,8,9,10};
-        Summer summer=new Summer(nums, 0, nums.length-1);
-        System.out.println(ForkJoinPool.commonPool().invoke(summer));
-     }
-
-}
-
-
-class Summer extends RecursiveTask<Integer>{
-
-    private final int nums[];
-    private final int from;
-    private final int to;
-    private final int thrshHold=3;
-    
-
-    public Summer(int[] nums, int from, int to) {
-        this.nums = nums;
-        this.from = from;
-        this.to = to;
-    }
-
-
-
-    @Override
-    protected Integer compute() {
-        int len=to-from;
-        if(len<=thrshHold){
-            int sum=0;
-            for(int i=from;i<=to;i++) sum+=nums[i];
-            return sum;
+    public static void main(String[] args) {
+        ThreadFactory tf=Thread.ofVirtual().name("worker-", 0).factory();
+        ExecutorService ser=Executors.newVirtualThreadPerTaskExecutor();
+        // ExecutorService ser=Executors.newThreadPerTaskExecutor(tf);
+        for(int i=0;i<10;i++){
+            final int j=i;
+            ser.submit(()->{
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName()+" "+j);
+            });
         }
-        int mid=(from+to)>>1;
-        Summer left=new Summer(nums, from, mid);
-        Summer right=new Summer(nums, mid+1, to);
-        left.fork();
-        right.fork();
-        int leftResult=left.join();System.out.println("["+from+","+mid+" via "+Thread.currentThread()+"]");
-        int rightResult=right.join();System.out.println("["+mid+","+to+" via "+Thread.currentThread()+"]");
-        return leftResult+rightResult;
+
+        System.out.println("hi");
+
+        ser.close();
     }
-    
 }
