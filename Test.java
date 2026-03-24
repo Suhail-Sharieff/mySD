7e0c559e0ede14e15b78ef6bde9759b9adffb020
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-03-24 20:30:48 +0530

    thenApply() vs thenCompose()

--- Test.java
+++ Test.java
@@ -1,27 +1,47 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-
+import java.lang.reflect.AnnotatedWildcardType;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 public class Test {
 
-    public static void main(String[] args) {
-        ThreadFactory tf=Thread.ofVirtual().name("worker-", 0).factory();
-        ExecutorService ser=Executors.newVirtualThreadPerTaskExecutor();
-        // ExecutorService ser=Executors.newThreadPerTaskExecutor(tf);
-        for(int i=0;i<10;i++){
-            final int j=i;
-            ser.submit(()->{
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(Thread.currentThread().getName()+" "+j);
-            });
-        }
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        
 
-        System.out.println("hi");
 
-        ser.close();
+        // CompletableFuture<CompletableFuture<Address>> x=getUser(23)
+        // .thenApply(user->getUserLocation(user));
+        CompletableFuture<Address> x=getUser(23)
+        .thenCompose(user->getUserLocation(user));
+
+
+        System.out.println(x.getClass().descriptorString());
+
+
+        x.join();   
+
+
     }
+
+
+    static CompletableFuture<User> getUser(int id){ return CompletableFuture.supplyAsync(()->new User(id));}
+    static CompletableFuture<Address> getUserLocation(User u){return CompletableFuture.supplyAsync(()->new Address(u));}
+    static CompletableFuture<Order> getOrders(Address addr){return CompletableFuture.supplyAsync(()->new Order(addr));}
+
+    
+
+
+    
+    static void sleep(int dur){try{System.out.println(Thread.currentThread());Thread.sleep(dur);}catch(InterruptedException ex){}}
+}
+
+
+class User{
+    public User(int id){}
+}
+
+class Address{
+    public Address(User u){}
+}
+class Order{
+    public Order(Address addr){}
 }
