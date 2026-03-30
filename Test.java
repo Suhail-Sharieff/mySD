21c13fde8ab9c5fde1cc97c378d94af7260e7d33
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-03-30 17:15:41 +0530

    blocking q & priority blocking q

--- Test.java
+++ Test.java
@@ -1,35 +1,47 @@
-import java.util.function.Supplier;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class Test {
 
     public static void main(String[] args) throws InterruptedException {
-
-        Supplier<Integer>sp=()->23;
-
-
-        ThreadLocal<Integer>tl=ThreadLocal.withInitial(sp);
-
-
-
-        Thread t1=new Thread(()->{
-            tl.set(23000);System.out.println(tl.get());try {
-                tl.remove();
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }System.out.println(tl.get());
+        BlockingQueue<Integer> bq=new ArrayBlockingQueue<>(3);
+        
+        
+        Thread producer=new Thread(()->{
+            for(int i=0;i<10;i++){
+                try {
+                    bq.put(i);//blocks if more elemnts than cap
+                    System.out.println("produced "+i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+         
+        Thread consumer=new Thread(()->{
+            for(int i=10;i<20;i++){
+                try {
+                    System.out.println(bq.take());//blocks if q is empty
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try{Thread.sleep(1000);}catch(InterruptedException ex){}
+            }
         });
 
-        Thread t2=new Thread(()->{tl.set(34000);System.out.println(tl.get());});
+        //
 
 
-        t1.start();
-        t2.start();
+        producer.start();
+        Thread.sleep(3000);
+        consumer.start();
 
+        producer.join();;
+        consumer.join();
 
-        t1.join();;
-        t2.join();;
+        System.out.println(bq);
+
 
     }
 }
