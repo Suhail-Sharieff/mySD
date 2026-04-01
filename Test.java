d011b69850d64a92942864b772cf754b32249263
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-04-01 12:08:31 +0530

    test all of fan out

--- Test.java
+++ Test.java
@@ -1,104 +1,35 @@
-import java.util.Random;
-import java.util.concurrent.*;
-import java.util.function.Supplier;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        Random rand = new Random();
+        List<CompletableFuture<Integer>> li = List.of(
+                get(300),
+                get(1000),
+                get(4000));
 
-        Callable<Integer> callable = () -> {
-            int r = rand.nextInt(4990, 6000);
-            System.out.println(r);
-            if (r % 2 == 0)
-                throw new RuntimeException("Some server error occured!");
-            else {
-                sleep(r);
-                return 1;
-            }
-        };
+        CompletableFuture<List<Integer>>cf=CompletableFuture.allOf(li.toArray(value -> new CompletableFuture[0])).thenApply(x -> {
+           return li.stream().map(e->e.join()).toList();
+        })
+        ;
 
-        Supplier<CompletableFuture<Integer>> supplier = () -> {
-            return CompletableFuture.supplyAsync(() -> {
-                try {
-                    return callable.call();
-                } catch (Exception ex) {
-                    throw new RuntimeException(ex);
-                }
-            });
-        };
-
-        ScheduledExecutorService es = Executors.newScheduledThreadPool(2);
-
-        CompletableFuture<Integer>cf=retry_with_exponential_backoff(3, es, 1000, 2, 5000, supplier);
-
-        try{
-            cf.thenAccept(v->System.out.println(v)).exceptionally(ex->{System.out.println(ex);return null;});
-            cf.join();
-        }catch(Exception e){
-            // System.out.println(e);
-        }finally{
-            es.shutdown();
-            es.close();;
-        }
-
+        cf.thenAccept(x->System.out.println(x)).join();
 
     }
-static <T> CompletableFuture<T> retry_with_exponential_backoff(
-        int retries,
-        ScheduledExecutorService es,
-        long delay,
-        int backoff,
-        long timeout,
-        Supplier<CompletableFuture<T>> supplier) {
 
-    CompletableFuture<T> result = new CompletableFuture<>();
-
-    attempt(result, retries, es, delay, backoff, timeout, supplier);
-
-    return result;
-}
-
-static <T> void attempt(
-        CompletableFuture<T> result,
-        int retries,
-        ScheduledExecutorService es,
-        long delay,
-        int backoff,
-        long timeout,
-        Supplier<CompletableFuture<T>> supplier) {
-
-    es.schedule(() -> {
-        supplier.get()
-            .orTimeout(timeout, TimeUnit.MILLISECONDS)
-            .whenComplete((res, ex) -> {
-
-                if (ex == null) {
-                    result.complete(res);
-                    return;
-                }
-
-                if (retries <= 0) {
-                    result.completeExceptionally(ex);
-                    return;
-                }
-
-                System.out.println("Retrying after delay: " + delay);
-
-                attempt(result, retries - 1, es,
-                        delay * backoff, backoff, timeout, supplier);
-
-            });
-
-    }, delay, TimeUnit.MILLISECONDS);
-}
+    static CompletableFuture<Integer> get(long dur) {
+        return CompletableFuture.supplyAsync(() -> {
+            sleep(dur);
+            return (int) dur;
+        });
+    }
 
     static void sleep(long dur) {
         try {
             Thread.sleep(dur);
         } catch (InterruptedException ex) {
-            Thread.currentThread().interrupt();
         }
     }
 }
