c2aac9b3f5ea1b407a67973471c8945646488064
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-04-06 21:36:53 +0530

    state pattern rev

--- Test.java
+++ Test.java
@@ -1,52 +1,100 @@
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
-
-public class Test {
-
-    public static void main(String[] args) throws InterruptedException {
-        CyclicBarrier bar=new CyclicBarrier(3);
-
-        var t1=new Thread(()->{
-            sleep(3000);
-            try {
-                bar.await();//blocks THIS thread, untill other threads complee too
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (BrokenBarrierException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        });
-        var t2=new Thread(()->{sleep(1000);try {
-            bar.await();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (BrokenBarrierException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }});
-        var t3=new Thread(()->{sleep(2000);try {
-            bar.await();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (BrokenBarrierException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }});
+interface AtmBehaviour{
+    void step1();
+    void step2();
+    void step3();
+}
+class Atm{
+    AtmBehaviour currState;
+    public Atm(){this.currState=new FirstState(this);}
+    void step1(){currState.step1();}
+    void step2(){currState.step2();}
+    void step3(){currState.step3();}
+    void setState(AtmState newState){currState=newState;}
+}
+abstract class AtmState implements AtmBehaviour{
+    protected final Atm atm;
+    public AtmState(Atm mc) {
+        this.atm=mc;
+    }
+}
 
 
-        t1.start();t2.start();t3.start();
-       
+class FirstState extends AtmState{
 
-        t1.join();
-        t2.join();
-        t3.join();
-
+    public FirstState(Atm mc) {
+        super(mc);
     }
 
+    @Override
+    public void step1() {
+        System.out.println("Step 1 complete");
+        atm.setState(new SecondState(atm));
+    }
 
-    static void sleep(long dur){try{Thread.sleep(dur);}catch(InterruptedException ex){}}
+    @Override
+    public void step2() {
+        System.out.println("Complete step1 firt");
+    }
+
+    @Override
+    public void step3() {
+        System.out.println("Complete step1 first");        
+    }
+}
+class SecondState extends AtmState{
+
+    public SecondState(Atm mc) {
+        super(mc);
+    }
+
+    @Override
+    public void step1() {
+        System.out.println("Step 1 already done");
+    }
+
+    @Override
+    public void step2() {
+        System.out.println("step2 complete");
+        atm.setState(new ThirdState(atm));
+    }
+
+    @Override
+    public void step3() {
+        System.out.println("Complete step2 first");        
+    }
+}
+class ThirdState extends AtmState{
+
+    public ThirdState(Atm mc) {
+        super(mc);
+    }
+
+    @Override
+    public void step1() {
+        System.out.println("Step 1 already done");
+    }
+
+    @Override
+    public void step2() {
+        System.out.println("Step 2 already done");
+    }
+
+    @Override
+    public void step3() {
+        System.out.println("step3 complete");        
+        atm.setState(new FirstState(atm));
+    }
+}
+public class Test {
+
+    public static void main(String[] args) {
+        Atm atm=new Atm();
+
+        atm.step1();
+        atm.step2();
+        atm.step3();
+
+        atm.step2();;
+
+    }
 }
