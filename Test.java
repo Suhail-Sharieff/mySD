c6fc6173fd772f70527b34d91fb988d25f351eb4
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-04-07 10:11:29 +0530

    rev visitor

--- Test.java
+++ Test.java
@@ -1,67 +1,41 @@
-//chain of responsibuliyy
-
-
-interface MiddlewareFunc{
-    boolean pass(String s);
+interface Shape{
+    double accept(ShapeVisitor v);
 }
 
-abstract class Middleware implements MiddlewareFunc{
-    protected Middleware nxt;
-    public boolean check(String s){
-        if(nxt==null) return pass(s);
-        if(pass(s)) return nxt.check(s);
-        return false;
-    }
+interface ShapeVisitor{
+    double visitCircle(Circle c);
+    double visitRectangle(Rectangle r);
 }
 
 
-class Chck1 extends Middleware{
+class AreaVisitor implements ShapeVisitor{
+    @Override 
+    public double visitCircle(Circle c){return Math.PI*c.r*c.r;}
+    @Override 
+    public double visitRectangle(Rectangle r){return r.l*r.b;}
+}
 
+
+class Circle implements Shape{
+    double r;
+    public Circle(double r){this.r=r;}
     @Override
-    public boolean pass(String s) {
-        if(s.contains("key1")){
-            System.out.println("chk1 pass");
-            return true;
-        }
-        System.out.println("chk1 fail");
-        return false;
-    }
-
+    public double accept(ShapeVisitor s){return s.visitCircle(this);}
 }
-class Chck2 extends Middleware{
-
+class Rectangle implements Shape{
+    double l,b;
+    public Rectangle(double l,double b){this.l=l;this.b=b;}
     @Override
-    public boolean pass(String s) {
-        if(s.contains("key2")){
-            System.out.println("chk2 pass");
-            return true;
-        }
-        System.out.println("chk2 fail");
-        return false;
-    }
-
-}
-class Chck3 extends Middleware{
-    @Override
-    public boolean pass(String s) {
-        if(s.contains("key3")){
-            System.out.println("chk3 pass");
-            return true;
-        }
-        System.out.println("chk3 fail");
-        return false;
-    }
+    public double accept(ShapeVisitor s){return s.visitRectangle(this);}
 }
 
 public class Test {
 
     public static void main(String[] args) {
-        Middleware mw=new Chck1();
-        mw.nxt=new Chck2();
-        mw.nxt.nxt=new Chck3();
-
-        System.out.println(mw.check("key1 key2"));
-
-
+        Shape cir=new Circle(23);
+        Shape  rect=new Rectangle(2,3);
+        ShapeVisitor vis=new AreaVisitor();
+        System.out.println(cir.accept(vis));
+        System.out.println(rect.accept(vis));
     }
 }
