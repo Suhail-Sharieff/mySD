cc4d2879321de9ff690ae8134a1bfe02b6384105
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-04-12 09:30:05 +0530

    hehe

--- Test.java
+++ Test.java
@@ -2,9 +2,15 @@ import java.util.List;
 
 public class Test{
     public static void main(String[] args) {
-        List<String>li=List.of("1","2","3");
-        String ans=String.join(",", li);
-        System.out.println(List.of(ans.split(" "))  );
+        int a=3,b=4,c=5;
+        double cosA=(b*b+c*c-a*a)/(2d*b*c);
+        double angA=Math.acos(cosA);
+        double cosB=(a*a+c*c-b*b)/(2d*a*c);
+        double angB=Math.acos(cosB);
+
+        double angC=180d-(angA+angB);
+
+        System.out.println(angA+" "+angB+" "+angC);
 
     }
 }
