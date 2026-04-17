308afd969a8ad9bacae7da9e1e5af65d62753df7
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-04-17 12:42:32 +0530

    h20 sem+lock+cyclic barrier

---
+++ synchronization_problems/_03_FizzBuzz.java
@@ -0,0 +1,262 @@
+package synchronization_problems;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class _03_FizzBuzz {
+
+    // design decision:
+
+    // using semaphores can be complex coz we arent sure whom to ive next control to
+    // (fizz or buzz or fizzbuzz or number) coz we cant predixct if next number
+    // would be div by 3 or 5 o neither, since conditons are complex its btter to
+    // use conditiaonl variables
+
+    public static void main(String[] args) throws InterruptedException {
+        _03_FizzBuzz obj = new _03_FizzBuzz(10);
+        Thread fizz = new Thread(() -> {
+            try {
+                obj.fizz();
+            } catch (InterruptedException ex) {
+            }
+        });
+        Thread buzz = new Thread(() -> {
+            try {
+                obj.buzz();
+            } catch (InterruptedException ex) {
+            }
+        });
+        Thread fizzBuzz = new Thread(() -> {
+            try {
+                obj.fizzbuzz();
+            } catch (InterruptedException ex) {
+            }
+        });
+        Thread number = new Thread(() -> {
+            try {
+                obj.number();
+            } catch (InterruptedException ex) {
+            }
+        });
+
+        fizz.start();
+        buzz.start();
+        fizzBuzz.start();
+        number.start();
+
+        fizz.join();
+        buzz.join();
+        fizzBuzz.join();
+        number.join();
+
+    }
+
+    private int n;
+    private ReentrantLock lock;
+    private Condition cond;
+    private int curr;
+
+    public _03_FizzBuzz(int n) {
+        this.n = n;
+        this.lock = new ReentrantLock();
+        this.cond = this.lock.newCondition();
+        this.curr = 1;
+    }
+
+    // printFizz.run() outputs "fizz". 3 ~5
+    public void fizz() throws InterruptedException {
+        while (curr <= n) {// can be replaced with `while(true)` as well
+            lock.lock();
+            try {
+                while (curr <= n && !(curr % 3 == 0 && curr % 5 != 0))// mISTAKE: wrote curr>n instaed of currM=n
+                    cond.await();
+                if (curr > n)
+                    return;// MISTAKE: i left this
+                System.out.print("fizz ");
+                curr++;
+                cond.signalAll();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz". 5 ~3
+    public void buzz() throws InterruptedException {
+        while (curr <= n) {
+            lock.lock();
+            try {
+                while (curr <= n && !(curr % 5 == 0 && curr % 3 != 0))
+                    cond.await();
+                if (curr > n)
+                    return;
+                System.out.print("buzz ");
+                curr++;
+                cond.signalAll();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz". 3 5
+    public void fizzbuzz() throws InterruptedException {
+        while (curr <= n) {
+            lock.lock();
+            try {
+                while (curr <= n && !(curr % 3 == 0 && curr % 5 == 0))
+                    cond.await();
+                if (curr > n)
+                    return;
+                System.out.print("fizzbuzz ");
+                curr++;
+                cond.signalAll();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer. either
+    public void number() throws InterruptedException {
+        while (curr <= n) {
+            lock.lock();
+            try {
+                while (curr <= n && (curr % 3 == 0 || curr % 5 == 0))
+                    cond.await();// mISTAKE: wrote && instaed of ||
+                if (curr > n)
+                    return;
+                System.out.print(curr + " ");
+                curr++;
+                cond.signalAll();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    // --------------------------------Semaphore based solution: so wt wre we re
+    // thinking is how to release particular sepahore,what if we create a function
+    // that decides which sem to reease based on value of curr??
+
+    private static class SemaphoreBAsedSoln {
+
+        public static void main(String[] args) throws InterruptedException {
+            SemaphoreBAsedSoln obj=new SemaphoreBAsedSoln(10);
+            Thread fizz = new Thread(() -> {
+                try {
+                    obj.fizz();
+                } catch (InterruptedException ex) {
+                }
+            });
+            Thread buzz = new Thread(() -> {
+                try {
+                    obj.buzz();
+                } catch (InterruptedException ex) {
+                }
+            });
+            Thread fizzBuzz = new Thread(() -> {
+                try {
+                    obj.fizzbuzz();
+                } catch (InterruptedException ex) {
+                }
+            });
+            Thread number = new Thread(() -> {
+                try {
+                    obj.number();
+                } catch (InterruptedException ex) {
+                }
+            });
+
+            fizz.start();
+            buzz.start();
+            fizzBuzz.start();
+            number.start();
+
+            fizz.join();
+            buzz.join();
+            fizzBuzz.join();
+            number.join();
+        }
+
+        private final int n;
+        private int curr;
+
+        private final Semaphore fizz = new Semaphore(0);
+        private final Semaphore buzz = new Semaphore(0);
+        private final Semaphore fizzbuzz = new Semaphore(0);
+        private final Semaphore number = new Semaphore(0);
+
+        public SemaphoreBAsedSoln(int n) {
+            this.n = n;
+            curr = 1;
+            signalCorrectSemaphore();
+        }
+
+        public void signalCorrectSemaphore() {
+            if (curr > n) {
+                // signal All to terminate (coz they call break on crr>n)
+                fizz.release();
+                buzz.release();
+                fizzbuzz.release();
+                number.release();
+            } else {
+                if (curr % 3 == 0 && curr % 5 == 0)
+                    fizzbuzz.release();
+                else if (curr % 3 == 0 && curr % 5 != 0)
+                    fizz.release();
+                else if (curr % 3 != 0 && curr % 5 == 0)
+                    buzz.release();
+                else
+                    number.release();
+            }
+        }
+
+        public void fizz() throws InterruptedException {
+            while (curr <= n) {//can use true instead of curr<=n
+                fizz.acquire();
+                if (curr > n)//IMP: coz curr may hv been exceeded n by other threads
+                    break;
+                System.out.print("fizz ");
+                curr++;
+                signalCorrectSemaphore();
+            }
+        }
+
+        public void buzz() throws InterruptedException {
+            while (curr <= n) {
+                buzz.acquire();
+                if (curr > n)
+                    break;
+                System.out.print("buzz ");
+                curr++;
+                signalCorrectSemaphore();
+            }
+        }
+
+        public void fizzbuzz() throws InterruptedException {
+            while (curr <= n) {
+                fizzbuzz.acquire();
+                if (curr > n)
+                    break;
+                System.out.print("fizzbuzz ");
+                curr++;
+                signalCorrectSemaphore();
+            }
+        }
+
+        public void number() throws InterruptedException {
+            while (curr <= n) {
+                number.acquire();
+                if (curr > n)
+                    break;
+                System.out.print(curr + " ");
+                curr++;
+                signalCorrectSemaphore();
+            }
+        }
+
+    }
+
+}
---
+++ synchronization_problems/_04_H20.java
@@ -0,0 +1,215 @@
+package synchronization_problems;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class _04_H20 {
+
+    public static void main(String[] args) throws InterruptedException {
+        UsingSemaphrores.main(args);System.out.println();
+        UsingCyclicBarrier.main(args);System.out.println();
+        UsingLocks.main(args);System.out.println();
+    }
+
+    // -----------------------usingSempa: prints 2H+10 always: HHO HHO HHO
+    private static class UsingSemaphrores {
+        private final Semaphore hydrogen = new Semaphore(2);
+        private final Semaphore oxygen = new Semaphore(0);
+
+        public void hydrogen() throws InterruptedException {
+            hydrogen.acquire();
+            System.out.print("H");
+            oxygen.release();
+        }
+
+        public void oxygen() throws InterruptedException {
+            oxygen.acquire(2);// wait for 2 oxygens to be released by hydrogen
+            System.out.print("O");
+            hydrogen.release(2);// free 2 hydrogens next
+        }
+
+        public static void main(String[] args) throws InterruptedException {
+            UsingSemaphrores obj = new UsingSemaphrores();
+            int nh = 6;
+            int no = 3;
+            Thread hydrogens[] = new Thread[nh];
+            Thread oxygens[] = new Thread[no];
+
+            for (int i = 0; i < nh; i++)
+                hydrogens[i] = new Thread(() -> {
+                    try {
+                        obj.hydrogen();
+                    } catch (InterruptedException ex) {
+                    }
+                });
+            for (int i = 0; i < no; i++)
+                oxygens[i] = new Thread(() -> {
+                    try {
+                        obj.oxygen();
+                    } catch (InterruptedException ex) {
+                    }
+                });
+
+            for (Thread h : hydrogens)
+                h.start();
+            for (Thread o : oxygens)
+                o.start();
+
+            for (Thread h : hydrogens)
+                h.join();
+            for (Thread o : oxygens)
+                o.join();
+
+        }
+
+    }
+
+    // --------------using Cycylic Barrier: prints 2H and 1O but order is not
+    // gurenteed ie it can HOH, OHH ....etc
+    private static class UsingCyclicBarrier {
+        public static void main(String[] args) throws InterruptedException {
+            UsingCyclicBarrier obj = new UsingCyclicBarrier();
+            int nh = 6;
+            int no = 3;
+            Thread hydrogens[] = new Thread[nh];
+            Thread oxygens[] = new Thread[no];
+
+            for (int i = 0; i < nh; i++)
+                hydrogens[i] = new Thread(() -> {
+                    try {
+                        obj.hydrogen();
+                    } catch (Exception ex) {
+                    }
+                });
+            for (int i = 0; i < no; i++)
+                oxygens[i] = new Thread(() -> {
+                    try {
+                        obj.oxygen();
+                    } catch (Exception ex) {
+                    }
+                });
+
+            for (Thread h : hydrogens)
+                h.start();
+            for (Thread o : oxygens)
+                o.start();
+
+            for (Thread h : hydrogens)
+                h.join();
+            for (Thread o : oxygens)
+                o.join();
+
+        }
+
+        public UsingCyclicBarrier() {
+            hydrogen = new Semaphore(2);
+            oxygen = new Semaphore(1);// see NOT 0
+            barrier = new CyclicBarrier(3, () -> {
+                // when any 3 threads(any combo of H and O ) hit barrier do this
+                hydrogen.release(2);
+                oxygen.release(1);
+            });
+        }
+
+        private final CyclicBarrier barrier;
+        private final Semaphore hydrogen;
+        private final Semaphore oxygen;
+
+        public void hydrogen() throws InterruptedException, BrokenBarrierException {
+            hydrogen.acquire();// ensures exactly 2 H enters inside
+            System.out.print("H");
+            barrier.await();
+            hydrogen.release();
+        }
+
+        public void oxygen() throws InterruptedException, BrokenBarrierException {
+            oxygen.acquire();// exacly 1 O enters here
+            System.out.print("O");
+            barrier.await();
+            oxygen.release();
+        }
+
+        
+
+    }
+        // ------------------------Using Locks: HHO HHO pattern:
+    private static class UsingLocks {
+
+            public static void main(String[] args) throws InterruptedException {
+                UsingLocks obj = new UsingLocks();
+                int nh = 6;
+                int no = 3;
+                Thread hydrogens[] = new Thread[nh];
+                Thread oxygens[] = new Thread[no];
+
+                for (int i = 0; i < nh; i++)
+                    hydrogens[i] = new Thread(() -> {
+                        try {
+                            obj.hydrogen(()->System.out.print("H"));
+                        } catch (Exception ex) {
+                        }
+                    });
+                for (int i = 0; i < no; i++)
+                    oxygens[i] = new Thread(() -> {
+                        try {
+                            obj.oxygen(()->System.out.print("O"));
+                        } catch (Exception ex) {
+                        }
+                    });
+
+                for (Thread h : hydrogens)
+                    h.start();
+                for (Thread o : oxygens)
+                    o.start();
+
+                for (Thread h : hydrogens)
+                    h.join();
+                for (Thread o : oxygens)
+                    o.join();
+
+            }
+
+            int i;
+            Lock lock;
+            Condition c;
+
+            public UsingLocks() {
+                lock = new ReentrantLock();
+                c = lock.newCondition();
+                i = 0;
+            }
+
+            public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+                lock.lock();
+                try {
+                    while (i == 2)
+                        c.await();
+                    // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+                    releaseHydrogen.run();
+                    i = (i + 1) % 3;
+                    c.signalAll();
+                } finally {
+                    lock.unlock();
+                }
+            }
+
+            public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+                lock.lock();
+                try {
+                    while (i == 0 || i == 1)
+                        c.await();
+                    // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+                    releaseOxygen.run();
+                    i = (i + 1) % 3;
+                    c.signalAll();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+}
